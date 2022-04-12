bc706a8017310cb9adafb5d9fbb9d27a47087aca
Deyvid Arauz <deyvida1001@gmail.com>
2022-04-12 15:48:29 -0600

    Initial config + NavBar

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,53 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Image } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import FavoriteScreen from "../screens/Favorites";
+import PokedexScreen from "../screens/Pokedex";
+import AccountScreen from "../screens/Account";
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Favorites"
+        component={FavoriteScreen}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={PokedexScreen}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: () => renderPokeball(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
+        options={{
+          tabBarLabel: "Mi Cuenta",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function renderPokeball() {
+  return (
+    <Image
+      source={require("../assets/pokeball.png")}
+      style={{ width: 75, height: 75, top: -20 }}
+    />
+  );
+}
---
+++ src/screens/Favorites.js
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+
+export default function Favorites() {
+  return (
+    <SafeAreaView>
+      <Text>Favorites</Text>
+    </SafeAreaView>
+  );
+}
