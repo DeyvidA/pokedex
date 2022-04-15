0c518356581c8388cea55c83b300daf19b49379c
Deyvid Arauz <deyvida1001@gmail.com>
2022-04-15 15:46:40 -0600

    Form system

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "darauz",
+  password: "zawarudo",
+};
+
+export const userDetails = {
+  userName: "darauz",
+  firstName: "Deyvid",
+  lastName: "Arauz",
+  email: "daruz@nicarao.agency",
+};
